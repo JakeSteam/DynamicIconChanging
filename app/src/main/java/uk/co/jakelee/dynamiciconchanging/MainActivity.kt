41e0743446e8666599ace6c44ef4f9d5a9073739
Jake Lee <github@jakelee.co.uk>
2019-04-28 19:55:51 +0100

    Minor tidying

--- app/src/main/java/uk/co/jakelee/dynamiciconchanging/MainActivity.kt
+++ app/src/main/java/uk/co/jakelee/dynamiciconchanging/MainActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-
-
 class MainActivity : AppCompatActivity() {
 
     enum class ICON_COLOUR { RED, GREEN, BLUE }
@@ -22,18 +20,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun setIcon(targetColour: ICON_COLOUR) {
         for (value in ICON_COLOUR.values()) {
+            val action = if (value == targetColour) {
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+            } else {
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+            }
             packageManager.setComponentEnabledSetting(
                 ComponentName(BuildConfig.APPLICATION_ID, "${BuildConfig.APPLICATION_ID}.${value.name}"),
-                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
+                action, PackageManager.DONT_KILL_APP
             )
-        }
-        for (value in ICON_COLOUR.values()) {
-            if (value != targetColour) {
-                packageManager.setComponentEnabledSetting(
-                    ComponentName(BuildConfig.APPLICATION_ID, "${BuildConfig.APPLICATION_ID}.${value.name}"),
-                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
-                )
-            }
         }
     }
 }
